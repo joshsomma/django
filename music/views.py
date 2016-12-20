3366548963a59871a07ef33872d086f25ab26a01
Josh S <josh@joshsomma.com>
2016-12-20 15:32:07 +0000

    updates

--- music/views.py
+++ music/views.py
@@ -1,15 +1,22 @@
-from django.http import HttpResponse
+from django.http import Http404
+#from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Album
 
 def index(request):
     #connect to dbase
     all_albums = Album.objects.all()
-
-    html = ''
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br />'
-    return HttpResponse(html)
+    #create html tmpl
+    #template = loader.get_template('music/index.html')
+    #context = {'all_albums' : all_albums}
+    #return HttpResponse(template.render(context,request))
+    return render(request,'music/index.html', {'all_albums' : all_albums})
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    #return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request,'music/detail.html', {'album' : album})
