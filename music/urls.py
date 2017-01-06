d16b7e8be05c5fd473f8b6cc40ce898ba87b6760
Josh S <josh@joshsomma.com>
2017-01-06 09:27:35 +0000

    currently on lesson 23

--- music/urls.py
+++ music/urls.py
@@ -2,10 +2,15 @@
 from django.conf.urls import include, url
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     # /music/
     url(r'^$', views.index, name="index"),
 
-    # /music/71
+    # /music/71/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
+
+    # /music/71/favourite
+    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name="favourite"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
-from django.http import Http404
+#from django.http import Http404
 #from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 def index(request):
@@ -14,9 +14,9 @@ def index(request):
 
 def detail(request, album_id):
     #return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
-    try:
-        album = Album.objects.get(pk=album_id)
-
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    album = get_object_or_404(Album, pk=album_id)
+    #try:
+        #album = Album.objects.get(pk=album_id)
+    #except Album.DoesNotExist:
+        #raise Http404("Album does not exist")
     return render(request,'music/detail.html', {'album' : album})
