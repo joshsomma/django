9503573b43e0d08473aecdaf8dd70441d2705050
Josh S <josh@joshsomma.com>
2017-01-11 15:12:31 +0000

    finished lesson 29

--- music/urls.py
+++ music/urls.py
@@ -6,11 +6,11 @@ app_name = 'music'
 
 urlpatterns = [
     # /music/
-    url(r'^$', views.index, name="index"),
+    url(r'^$', views.IndexView.as_view(), name="index"),
 
     # /music/71/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name="detail"),
 
-    # /music/71/favourite
-    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name="favourite"),
+    # /music/71/favourite deleted per lesson 28
+    # url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name="favourite"),
 ]
--- music/views.py
+++ music/views.py
@@ -1,28 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Album, Song
+from django.views import generic
+from .models import Album
 
-def index(request):
-    #connect to dbase
-    all_albums = Album.objects.all()
-    #use render to get list of all albums
-    return render(request,'music/index.html', {'all_albums' : all_albums})
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
-def detail(request, album_id):
-    #check and see if an album w id is available or return 404 msg
-    album = get_object_or_404(Album, pk=album_id)
-    #use render to get album details
-    return render(request,'music/detail.html', {'album' : album})
+    def get_queryset(self):
+        return Album.objects.all()
 
-def favourite (request,album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-    except (KeyError, Song.DoesNotExist):
-        return render(request,'music/detail.html', {
-        'album':album,
-        'error_message': "You did not select a valid song"
-        })
-    else:
-        selected_song.is_favourite = True
-        selected_song.save()
-        return render(request,'music/detail.html', {'album' : album})
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
